610c141e9438a4367cd0d8b8e88fe69b1b2cb5a5
Anjali Sureddi <sunnymsd39@fmail.com>
2024-03-11 21:01:19 +0530

    my portfolio

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,23 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/pic3.jpg" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+          Hey there! I'm Sunny Likhith, a passionate frontend and full stack developer on an exciting journey through the ever-evolving landscape of technology.
+
+ With a keen eye for design and a love for crafting seamless user experiences, I specialize in building modern web applications that not only look great but also perform exceptionally well across various platforms and devices.
+          </p>
+          <p className="hero--section-description">
+          My toolkit includes a diverse range of technologies such as HTML5, CSS3 (including preprocessors like Sass), JavaScript (ES6+), and popular frontend frameworks like React.js. On the backend, I'm Working on Node.js, Express.js, and databases such as MongoDB and SQL.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/ContactMe.jsx
@@ -0,0 +1,12 @@
+export default function ContactMe() {
+  return (
+    <section id="Contact" className="contact--section">
+      <div>
+        <p className="sub--title">Get In Touch</p>
+        <h2>Contact Me</h2>
+        <h4>My Address: Vanapalli babanagar East godavari District,Andhra Pradesh.</h4>
+        <h4>phone number:+919398787881</h4>
+      </div>
+    </section>
+  );
+}
